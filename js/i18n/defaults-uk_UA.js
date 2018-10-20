b84b1d1afa79dd07c1128dff2757275267a874ff
Oleh Korotych <KorotychOleh@gmail.com>
2018-10-20 14:42:01 +0300

    Rename defaults-ua_UA.js to defaults-uk_UA.js
    Ukrainian ISO 639-1 code is "uk" not "ua".

--- js/i18n/defaults-uk_UA.js
+++ js/i18n/defaults-uk_UA.js
@@ -1,13 +1,13 @@
 /*
  * Translated default messages for bootstrap-select.
- * Locale: UA (Ukrainian; Українська)
+ * Locale: UK (Ukrainian; Українська)
  * Region: UA (Ukraine)
  */
 (function ($) {
   $.fn.selectpicker.defaults = {
     noneSelectedText: 'Нічого не вибрано',
     noneResultsText: 'Збігів не знайдено {0}',
-    countSelectedText: 'Вибрано {0} із {1}',
+    countSelectedText: 'Вибрано {0} з {1}',
     maxOptionsText: ['Досягнута межа ({n} {var} максимум)', 'Досягнута межа в групі ({n} {var} максимум)', ['items', 'item']],
     multipleSeparator: ', ',
     selectAllText: 'Вибрати все',
